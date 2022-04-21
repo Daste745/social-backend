8290453e4a66c62da51e4a870614b854a39b6b64
Daste <stefankar1000@gmail.com>
2022-04-21 23:04:36 +0200

    fix(users): make all fields in updateUserDto optional

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsEmail()
   email?: string;
 
+  @IsOptional()
   @IsString()
   password?: string;
 }
