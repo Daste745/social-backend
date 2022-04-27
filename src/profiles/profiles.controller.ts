e4a4bcca5d9bb55a82e032720c8c084102853d00
Daste <stefankar1000@gmail.com>
2022-04-27 19:16:55 +0200

    feat(profiles): add controllers for profile routes

---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,74 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Request,
+  UseGuards,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { ProfilesService } from './profiles.service';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { plainToInstance } from 'class-transformer';
+import { ReadProfileDto } from './dto/read-profile.dto';
+
+@Controller('profiles')
+export class ProfilesController {
+  constructor(private readonly profilesService: ProfilesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(
+    @Request() req,
+    @Body() createProfileDto: CreateProfileDto,
+  ): Promise<ReadProfileDto> {
+    try {
+      const profile = await this.profilesService.create(
+        createProfileDto,
+        req.user,
+      );
+      return plainToInstance(ReadProfileDto, profile);
+    } catch (_) {
+      throw new BadRequestException(
+        'This username is already in use by another user.',
+      );
+    }
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<ReadProfileDto> {
+    try {
+      const profile = await this.profilesService.findOne(id);
+      return plainToInstance(ReadProfileDto, profile);
+    } catch (_) {
+      throw new NotFoundException();
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  async update(
+    @Request() req,
+    @Param('id') id: string,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    const profile = await this.profilesService.findOne(id);
+
+    try {
+      const updatedProfile = await this.profilesService.update(
+        profile,
+        updateProfileDto,
+      );
+      return plainToInstance(ReadProfileDto, updatedProfile);
+    } catch (_) {
+      throw new BadRequestException(
+        'This username is already in use by another user.',
+      );
+    }
+  }
+}
