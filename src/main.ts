87073c16354076607afa910008e27357a6c0e7e1
Daste <stefankar1000@gmail.com>
2022-05-13 09:37:45 +0200

    feat(): add a simple SwaggerUI configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { env } from 'process';
 import { AppModule } from './app.module';
 
@@ -9,6 +10,14 @@ async function bootstrap() {
   const port = parseInt(env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Social')
+    .setVersion('0.0.1')
+    .build();
+  const api_spec = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, api_spec);
+
   await app.listen(port);
 }
 bootstrap();
