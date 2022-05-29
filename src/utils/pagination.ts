e8182cd98c6377db393e68dc9916ea8247963459
Daste <stefankar1000@gmail.com>
2022-05-29 20:59:58 +0200

    feat(): annotate query param types for PaginationOptions

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -2,6 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
+const DEFAULT_PER_PAGE = 100;
+
 export interface Paginated<T> {
   page: number;
   perPage: number;
@@ -13,13 +15,23 @@ export interface Paginated<T> {
 export class PaginateOptions {
   // TODO: Automatically cast parameters to int from request
   //       For some reason the IsNumer and Type annotations don't do anything
-  @ApiProperty()
+  @ApiProperty({
+    required: false,
+    type: Number,
+    minimum: 0,
+    default: 0,
+  })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   page?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    required: false,
+    type: Number,
+    minimum: 0,
+    default: DEFAULT_PER_PAGE,
+  })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
@@ -28,7 +40,7 @@ export class PaginateOptions {
 
 export function paginate<T>(data: T[], options: PaginateOptions): Paginated<T> {
   const page = parseInt(options.page) || 0;
-  const perPage = parseInt(options.perPage) || 100;
+  const perPage = parseInt(options.perPage) || DEFAULT_PER_PAGE;
   const offset = page * perPage;
 
   return {
